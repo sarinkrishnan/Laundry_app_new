9a13e8b9cd01718f0d8fd40130af19419bdab17d
sarinkrishnan <sarinkrishnan99@gmail.com>
2024-05-27 16:39:09 +0530

    some changes

--- lib/extract_items/01-splash_page-prototype/01-textfeild_splash_pages.dart
+++ lib/extract_items/01-splash_page-prototype/01-textfeild_splash_pages.dart
@@ -5,38 +5,42 @@ class TextfeildText extends StatelessWidget {
   const TextfeildText({
     super.key,
     required this.text,
-    required this.controler,required this.validator,
-
+    //  this.controler,
+    this.validator,
+    this.onSaveddata,
+    this.type,
   });
   final String text;
-  final TextEditingController controler;
-  final String Function(String?) validator;
-
+  // /final TextEditingController? controler;
+  final String Function(String?)? validator;
+  final void Function(String?)? onSaveddata;
+  final TextInputType? type;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 24.w, right: 27.w),
       child: TextFormField(
+        keyboardType: type,
         validator: validator,
-          style: TextStyle(
-            color: Colors.black,
-          ),
-          controller: controler,
-          decoration: InputDecoration(
-            contentPadding:
-                EdgeInsets.only(left: 35.w, top: 23.h, bottom: 23.h),
-            filled: true,
-            fillColor: Color(0xffFFFFFF),
-            hintText: text,
-            hintStyle: TextStyle(
-                color: Color.fromARGB(108, 131, 145, 161),
-                fontWeight: FontWeight.w500,
-                fontSize: 15.sp),
-            border: OutlineInputBorder(
-                borderSide: BorderSide.none,
-                borderRadius: BorderRadius.circular(140.r)),
-          ),
+        style: TextStyle(
+          color: Colors.black,
         ),
+        onSaved: (v) => onSaveddata,
+        // controller: controler,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(left: 35.w, top: 23.h, bottom: 23.h),
+          filled: true,
+          fillColor: Color(0xffFFFFFF),
+          hintText: text,
+          hintStyle: TextStyle(
+              color: Color.fromARGB(108, 131, 145, 161),
+              fontWeight: FontWeight.w500,
+              fontSize: 15.sp),
+          border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(140.r)),
+        ),
+      ),
     );
   }
 }
--- lib/prototype-screen/1-row-splash-page/4-register-page.dart
+++ lib/prototype-screen/1-row-splash-page/4-register-page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
-import 'package:laundry_bin_app/controler/register.dart';
 import 'package:laundry_bin_app/extract_items/01-splash_page-prototype/01-textfeild_splash_pages.dart';
 import 'package:laundry_bin_app/extract_items/splashpage-back_white_button/splash-back_white_button.dart';
 import 'package:laundry_bin_app/prototype-screen/1-row-splash-page/5-wlcome_back-login.dart';
-import 'package:laundry_bin_app/prototype-screen/2-row-home-page/11-home-page.dart';
-import 'package:provider/provider.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -17,11 +15,9 @@ class RegisterPage extends StatefulWidget {
   State<RegisterPage> createState() => _RegisterPageState();
 }
 
-TextEditingController name = TextEditingController();
-TextEditingController e_mail = TextEditingController();
-TextEditingController password = TextEditingController();
-// TextEditingController phoneNumber = TextEditingController();
-
+String? name;
+String? e_mail;
+String? password;
 int? phoneNumber;
 
 class _RegisterPageState extends State<RegisterPage> {
@@ -66,10 +62,11 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
               Gap(90.h),
               TextfeildText(
+                type: TextInputType.name,
                 text: 'Enter Name',
-                controler: name,
+                onSaveddata: (p0) => name = p0!,
                 validator: (p0) {
-                  if (name.text.isEmpty) {
+                  if (name.toString().isEmpty) {
                     return 'Please enter your name';
                   } else {
                     throw '';
@@ -78,22 +75,23 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
               Gap(15.h),
               TextfeildText(
-                controler: e_mail,
+                onSaveddata: (value) {
+                  e_mail = value!;
+                },
                 text: 'Enter Email ID or Phone Number',
-                validator: (p0) {
-                  if (e_mail.text.isEmpty) {
-                    return 'Enter Email ID or Phone Number';
-                  } else {
-                    throw '';
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter the data'; 
                   }
+                  return '';
                 },
               ),
               Gap(15.h),
               TextfeildText(
                 text: 'Enter your password',
-                controler: password,
+                onSaveddata: (p0) => password = p0!,
                 validator: (p0) {
-                  if (password.text.isEmpty) {
+                  if (password.toString().isEmpty) {
                     return "please enter the password";
                   } else {
                     throw "";
@@ -101,31 +99,44 @@ class _RegisterPageState extends State<RegisterPage> {
                 },
               ),
               Gap(15.h),
-              Padding(
-                padding: EdgeInsets.only(left: 24.w, right: 27.w),
-                child: TextFormField(
-                  style: TextStyle(color: Colors.black),
-                  keyboardType: TextInputType.number,
-                  // controller: phoneNumber,
-                  onSaved: (value) => phoneNumber = int.parse(value!),
-                  decoration: InputDecoration(
-                      fillColor: Color(0xffFFFFFFFF),
-                      filled: true,
-                      labelText: 'Phone Number',
-                      contentPadding: EdgeInsets.only(left: 32.w, top: 45.h),
-                      labelStyle:
-                          TextStyle(color: Color.fromARGB(66, 57, 51, 51)),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(100.r))),
-                  validator: (value) {
-                    if (!RegExp(r'^[0-9]+$').hasMatch(value!)) {
-                      return 'Please enter a valid phone number';
-                    }
-                    return null;
-                  },
-                ),
-              ),
+              TextfeildText(
+                type: TextInputType.number,
+                text: 'Phone Number',
+                onSaveddata: (p0) => phoneNumber = int.parse(p0!),
+                validator: (p0) {
+                  if (phoneNumber.toString().isEmpty) {
+                    return ' please enter the phone number';
+                  } else {
+                    throw '';
+                  }
+                },
+              )
+              // Padding(
+              //   padding: EdgeInsets.only(left: 24.w, right: 27.w),
+              //   child: TextFormField(
+              //     style: TextStyle(color: Colors.black),
+              //     keyboardType: TextInputType.number,
+              //     // controller: phoneNumber,
+              //     onSaved: (value) => phoneNumber = int.parse(value!),
+              //     decoration: InputDecoration(
+              //         fillColor: Color(0xffFFFFFFFF),
+              //         filled: true,
+              //         labelText: 'Phone Number',
+              //         contentPadding: EdgeInsets.only(left: 32.w, top: 45.h),
+              //         labelStyle:
+              //             TextStyle(color: Color.fromARGB(66, 57, 51, 51)),
+              //         border: OutlineInputBorder(
+              //             borderSide: BorderSide.none,
+              //             borderRadius: BorderRadius.circular(100.r))),
+              //     validator: (value) {
+              //       if (!RegExp(r'^[0-9]+$').hasMatch(value!)) {
+              //         return 'Please enter a valid phone number';
+              //       }
+              //       return null;
+              //     },
+              //   ),
+              // ),
+              ,
               Gap(15.h),
               Center(
                 child: SizedBox(
@@ -133,6 +144,14 @@ class _RegisterPageState extends State<RegisterPage> {
                     width: 333.w,
                     child: ElevatedButton(
                       onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                backgroundColor: Colors.greenAccent,
+                                content: Text('Data saved ')),
+                          );
+                        }
                       },
                       child: Text(
                         'Register',
